3b3042ab0ddb58d2f7b441941553a215dc51b2a6
Justin Chadwell <me@jedevc.com>
2025-10-21 15:26:03 +0100

    wip
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- core/query.go
+++ core/query.go
@@ -133,6 +133,7 @@ type Server interface {
 
 	// Open a client's telemetry database.
 	ClientTelemetry(ctc context.Context, sessID, clientID string) (*clientdb.Queries, func() error, error)
+	EngineName() string
 }
 
 type queryKey struct{}
